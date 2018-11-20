0630e9ddf443ba58f33fe3dd95a5de357836da61
tuongpv <tuongpvth@gmail.com>
2018-11-20 14:08:34 +0700

    init

--- Html2Pdf/Html2PdfDocker/Startup.cs
+++ Html2Pdf/Html2PdfDocker/Startup.cs
@@ -65,19 +65,5 @@ namespace Html2PdfDocker
         }
 
     }
-    internal class CustomAssemblyLoadContext : AssemblyLoadContext
-    {
-        public IntPtr LoadUnmanagedLibrary(string absolutePath)
-        {
-            return LoadUnmanagedDll(absolutePath);
-        }
-        protected override IntPtr LoadUnmanagedDll(String unmanagedDllName)
-        {
-            return LoadUnmanagedDllFromPath(unmanagedDllName);
-        }
-        protected override Assembly Load(AssemblyName assemblyName)
-        {
-            throw new NotImplementedException();
-        }
-    }
+  
 }
